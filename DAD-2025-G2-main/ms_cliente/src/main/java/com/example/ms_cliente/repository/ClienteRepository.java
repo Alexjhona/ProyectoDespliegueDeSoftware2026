e231faa2ea27fe0bf3090020761ebc1f0477e11c
eo-start <edwarnhastonriddler@gmail.com>
2026-04-04 12:08:06 -0500

    Fix config-data for microservices and Eureka registration

--- DAD-2025-G2-main/ms_cliente/src/main/java/com/example/ms_cliente/repository/ClienteRepository.java
+++ DAD-2025-G2-main/ms_cliente/src/main/java/com/example/ms_cliente/repository/ClienteRepository.java
@@ -2,9 +2,13 @@ package com.example.ms_cliente.repository;
 
 import com.example.ms_cliente.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    boolean existsByDniOrRuc(String dniOrRuc);
+
+    @Query("SELECT COUNT(c) > 0 FROM Cliente c WHERE c.dniOrRuc = :dniOrRuc")
+    boolean existsByDniOrRuc(@Param("dniOrRuc") String dniOrRuc);
 }
